059c18b91b5d828cb1aeee88ff5fd51f3f882156
Arostegui Maranto <arosteguimaranto@outlook.com>
2022-10-16 23:37:38 -0500

    Iniciando commit

---
+++ app.js
@@ -0,0 +1,44 @@
+
+require('colors');
+
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer');
+
+const { Tareas } = require('./models/tareas');
+
+
+const main = async () => {
+
+    let opt = '';
+    const tareas = new Tareas();
+
+    do {
+        // //Imprimir el menu
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case '1':
+                //crear opcio/*  */n
+                const desc = await leerInput('Descripcion: ');
+                tareas.crearTarea(desc);
+                break;
+
+            case '2':
+                console.log(tareas.listadoArr);
+
+                break;
+
+        }
+
+        await pausa();
+
+
+
+    } while (opt !== '0');
+
+
+
+
+    //pausa();
+}
+
+main();
---
+++ helpers/inquirer.js
@@ -0,0 +1,104 @@
+const inquirer = require('inquirer');
+const { v4: uuidv4 } = require('uuid');
+ 
+require('colors');
+ 
+
+const preguntas = [
+    {
+        type: 'list',
+        name: 'opcion',
+        message: 'Que desea hacer?',
+        choices: [
+            {
+                value: '1',
+                name: `${'1'.blue}  Crear tarea`
+            },
+            {
+                value: '2',
+                name: `${'2'.blue}  Listar tareas `
+
+            },
+            {
+                value: '3',
+                name: `${'3'.blue}  Listar tareas completadas`
+
+            },
+            {
+                value: '4',
+                name: `${'4'.blue}  Listar Tareas pendientes`
+
+            },
+            {
+                value: '5',
+                name: `${'5'.blue}  Completar tarea(s)`
+
+            },
+            {
+                value: '6',
+                name: `${'6'.blue}  Borrar tarea`
+
+            },
+            {
+                value: '0',
+                name: `${'0'.red}  Salir`
+
+            },
+        ]
+
+    }
+];
+
+const inquirerMenu = async () => {
+
+    //console.clear();
+    console.log('============================'.green);
+    console.log('   Seleccione una opcion    '.rainbow);
+    console.log('============================\n'.green);
+
+    const {opcion} = await inquirer.prompt(preguntas);
+
+    return opcion;
+}
+
+ const pausa = async() => { uuidv4();
+    const question = [
+        {
+            type: 'input',
+            name: 'enter',
+            message: `Presione ${'enter'.rainbow} para continuar `
+        }
+    ];
+
+    console.log('\n');
+    await inquirer.prompt(question);
+ }
+
+ const leerInput = async (message) => {
+
+    const question = [
+        {
+            type: 'input',
+            name: 'desc',
+            message,
+            validate( value){
+                if(value.length === 0){
+                    return 'Por favor ingrese un valor';
+                }
+                return true;
+            }
+         }
+    ]
+
+    
+    const {desc} = await inquirer.prompt(question);
+    return desc;
+
+ }
+
+
+
+
+ module.exports = {
+    inquirerMenu, pausa, leerInput
+}
